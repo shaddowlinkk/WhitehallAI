928e062f3e40dd31d432ff647ea2cc3a01ea61a4
shaddowlinkk <nathan.vanbrunt@gmail.com>
2020-04-22 21:03:34 -0400

    ability to read from property files

---
+++ src/configeration.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class configeration {
+    private Properties prop;
+    private FileInputStream is;
+    public configeration(String fileName){
+        prop = new Properties();
+        try {
+            is = new FileInputStream(fileName);
+        } catch (FileNotFoundException e){
+
+        }
+        try {
+            prop.load(is);
+        }catch (IOException e){
+
+        }
+    }
+
+    public String getProp( String propName) {
+        return prop.getProperty(propName);
+    }
+}
